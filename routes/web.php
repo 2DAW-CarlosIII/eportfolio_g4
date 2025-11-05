5eec28f2ee8a00544f38c49b37b5931660ad82d7
Adrian Cantero <1832726@alu.murciaeduca.es>
2025-11-05 09:43:37 +0100

    modificacion de routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,38 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return "Pantalla principal";
 });
+
+Route::get('login', function(){
+    return "Login usuario";
+});
+
+Route::get('logout', function(){
+    return "Logout usuario";
+});
+
+Route::get('familias-profesionales', function(){
+    return "Listado familias profesionales";
+});
+
+Route::get('familias-profesionales/show/{id}', function($id){
+    return "Vista detalle familia profesional $id";
+}) -> where('id', '[0-9]+');
+
+Route::get('familias-profesionales/create', function(){
+    return "Añadir familia profesional";
+});
+
+Route::get('familias-profesionales/edit/{id}', function($id){
+    return "Modificar familia profesional $id";
+}) -> where('id', '[0-9]+');
+
+Route::get('perfil/{id?}', function($id = null){
+    if ($id) {
+        return "Visualizar el currículo de $id";
+    } else {
+        return "Visualizar el currículo propio";
+    }
+})-> where('id', '[0-9]+');
