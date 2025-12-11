b3343a7dc29badebb53936f8ef91535a6b9d3ffb
Alejandro Bermúdez <2060559@alu.murciaeduca.es>
2025-12-11 11:48:27 +0100

    autenticaciones y cambios en header y banner con información del grupo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,7 +27,7 @@ class DatabaseSeeder extends Seeder
         $this->call(CiclosFormativosTableSeeder::class);
 
         $this->call (FamiliasProfesionalesTableSeeder::class);
-        
+
         $this->call(ResultadosAprendizajeTableSeeder::class);
 
 
