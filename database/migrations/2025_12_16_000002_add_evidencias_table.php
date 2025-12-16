693692b7f99490e9de1e9b892c0cfb343957fe0c
Jaime <5514523@alu.murciaeduca.es>
2025-12-16 23:10:57 +0100

    tabla evidencias

---
+++ database/migrations/2025_12_16_000002_add_evidencias_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('evidencias', function (Blueprint $table) {
+            $table->id();
+            $table->integer('estudiante_id')->nullable();
+            $table->integer('tarea_id')->nullable();
+            $table->string('url');
+            $table->string('descripcion');
+            $table->enum('estado_validacion',['pendiente', 'validada', 'rechazada']) ->default('pendiente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('evidencias', function (Blueprint $table) {
+            $table->dropColumn('estudiante_id');
+            $table->dropColumn('tarea_id');
+            $table->dropColumn('url');
+            $table->dropColumn('descripcion');
+            $table->dropColumn('estado_validacion');
+        });
+    }
+};
