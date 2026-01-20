88f8f12b96ea6310671e062d0239c504e29c66ec
Denis <5945069@alu.murciaeduca.es>
2026-01-20 08:44:26 +0100

    Ejercicios de controladores de recursos con la migracion de la clave foranea

--- database/migrations/2025_12_03_000002_create_ciclos_formativos_table.php
+++ database/migrations/2025_12_03_000002_create_ciclos_formativos_table.php
@@ -7,19 +7,14 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     public function up(): void
-    {
+     {
         Schema::create('ciclos_formativos', function (Blueprint $table) {
             $table->id();
-
-            $table->foreignId('familia_profesional_id')
-                ->constrained('familias_profesionales')
-                ->cascadeOnDelete();
-
-            $table->string('nombre', 255);
-            $table->string('codigo', 50)->unique();
-            $table->enum('grado', ['medio', 'superior', 'basica', 'C.E. (superior)', 'C.E. (medio)'])->nullable();
-            $table->text('descripcion')->nullable();
-
+            $table -> string('familia_profesional_id')->nullable();
+            $table -> string('nombre', 255);
+            $table -> string('codigo', 50);
+            $table->enum('grado', ['medio', 'superior', 'basica', 'C.E. (superior)', 'C.E. (medio)']);
+            $table -> text('descripcion')->nullable();
             $table->timestamps();
         });
     }
